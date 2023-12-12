8ba462fa3283685fa7d42d696d6c5ed5fa5090c0
E-Levine <149699489+E-Levine@users.noreply.github.com>
2023-12-12 07:02:07 -0500

    Update WQ_data_compilation.R
    Added code for reading in data files from folder.

--- WQ_data_compilation.R
+++ WQ_data_compilation.R
@@ -14,6 +14,32 @@ pacman::p_load(plyr, tidyverse, readxl, #Df manipulation, basic summary
                install = TRUE) 
 #
 #
-#Set parameters
+####Compilation setup####
+#
+#Set parameters - run for each data source type
+Estuary_code <- c("LW") #Two letter estuary code
 Data_source <- c("Portal") #"Portal" or "WA" 
-#Need to continue.
+#
+#Years of data:
+Start_year <- c("2000")
+End_year <- c("2022")
+Split_files <- c("N") #Are the results/data files split due to size ("Y" or "N")?
+#
+#
+#
+#
+####Load files####
+#
+##Read in Excel site file
+Location_data <- as.data.frame(read_excel(paste0("../Water-Quality-Processing-Data/Data/Raw_data/", Estuary_code, "_", Data_source,"_Site data_", Start_year, "_", End_year,".xlsx"), na = c("NA", " ", "", "Z")))
+#
+#Read in Excel results file (for 1 file) - skip to next section if only 1 results file
+Results_data <- as.data.frame(read_excel(paste0("../Water-Quality-Processing-Data/Data/Raw_data/", Estuary_code, "_", Data_source,"_Results_", Start_year, "_", End_year,".xlsx"), na = c("NA", " ", "", "Z")))
+#Read in Excel results file (for 2 files)
+Results1 <- as.data.frame(read_excel(paste0("Raw_data/", Estuary_code, "_", Data_source,"_Results_", Start_year, "_", "YYYY",".xlsx"), na = c("NA", " ", "", "Z")))
+Results2 <- as.data.frame(read_excel(paste0("Raw_data/", Estuary_code, "_", Data_source,"_Results_", "YYYY", "_", End_year,".xlsx"), na = c("NA", " ", "", "Z")))
+Results_data <- rbind(Results1, Results2)
+#If more files are needed, copy and edit the proper number of Results# lines of code and make sure to add all versions to the Results_data <- rbind() line.
+#
+#
+#
