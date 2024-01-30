ed3ddd3bf9278678ab56614e908806b3206c2ce7
E-Levine <149699489+E-Levine@users.noreply.github.com>
2024-01-30 13:37:59 -0500

    Update WQ_data_selection.R
    Updated code to select stations based on 2 buffer zones around specified stations. Working on code for additional station point selection to include based on mapping output.

--- WQ_data_selection.R
+++ WQ_data_selection.R
@@ -36,28 +36,59 @@ WQ_data <- as.data.frame(read_excel(paste0("../Water-Quality-Processing-Data/Dat
 #
 #Use following code to limit stations to specified distance from points (i.e., stations)
 Station_locations <- as.data.frame(read_excel("../Water-Quality-Processing-Data/Data/Reference_data/Stations_area_selections.xlsx", na = c("NA", " ", "", "Z")))
+#Code to limit stations if list contains more than needed:
+
 #
 ##Estuary area  
 Estuary_area <- st_read(paste0("KML/", Estuary_code, ".kml"))
 plot(Estuary_area[2])
-#
+###State Outline
+FL_outline <- st_read("KML/FL_Outlines/FL_Outlines.shp")
+plot(FL_outline)
 #
 #
 ####Station selection by distance####
 #
-Distance <- c(200) #distance in meters
-WQ_data_t <- st_as_sf(WQ_data, coords = c(9,8), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+F_Distance <- c(1000) #distance in meters
+S_Distance <- c(2000) #secondary distance for additional search area of potential stations 
+WQ_data_t <- st_as_sf(WQ_data, coords = c(9,8), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
 #
-Stations_t <- st_as_sf(Station_locations, coords = c(3,4), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs")
-#
-#
-test <- WQ_data_t %>% mutate(Within_dist = lengths(st_is_within_distance(., Stations_t, dist = Distance)))
+Stations_t <- st_as_sf(Station_locations, coords = c(3,4), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
 #
 #
-#Need to convert to CRS of meters for buffer area. 
-tm_shape(Estuary_area) + #Estuary area
-               tm_polygons() + 
-              # tm_shape(Combined_data) + #Stations relation to estuary area
-              # tm_dots("KML", palette = c(In = "red", Out = "black"), size = 0.25, legend.show = TRUE,
-               #        popup.vars = c("StationID" = "MonitoringLocationIdentifier", "Latitude" = "LatitudeMeasure", "Longitude" = "LongitudeMeasure")) +
-               tm_shape(WQ_data_t) + tm_dots("Within_dist", size = 0.25, legend.show = TRUE)
+#
+#
+WQ_Stations <- data.frame() #create blank data.frame to fill 
+#
+for(i in 1:nrow(Stations_t)){
+  ##Create each buffer zone for all stations of interest
+  Stations_FD <- st_buffer(Stations_t[i,], F_Distance)
+  Stations_SD <- st_difference(st_buffer(Stations_t[i,], S_Distance), Stations_FD)
+  ##Find all points within each buffer
+  Points_FD <- st_filter(WQ_data_t, Stations_FD) %>% mutate(Station = i, Buffer = paste0(F_Distance, "m"))
+  Points_SD <- st_filter(WQ_data_t, Stations_SD) %>% mutate(Station = i, Buffer = paste0(S_Distance, "m"))
+  ##Join data for each buffer area
+  temp <- bind_rows(Points_FD, Points_SD)
+  ##Join into final output dataframe of all stations
+  WQ_Stations <- rbind(WQ_Stations, temp)
+}
+#
+WQ_Stations <- WQ_Stations %>% mutate(Station = as.factor(Station))
+#
+#
+#
+(map <- tmap_leaflet(tm_shape(Estuary_area) + tm_polygons(col = "lightblue")+ #Estuary area
+                    tm_shape(FL_outline) + tm_borders()+ #Outline of shoreline
+                    tm_shape(Stations_t) + tm_dots(size = 1, legend.show = TRUE)+ #All possible stations
+                    tm_shape(WQ_data_t) + tm_dots(popup.vars = c("StationID" = "MonitoringLocationIdentifier"))+ #Reference stations
+                    tm_shape(WQ_Stations) + tm_dots(col = "Station", size = 0.25, legend.show = TRUE,
+                                                    popup.vars = c("StationID" = "MonitoringLocationIdentifier", "Station" = "Station", "Buffer" = "Buffer")))) #Selected stations and buffer area
+#
+# List of any stations to include: need station ID and station of reference 
+To_include <- data.frame(StationID = c(),
+                         Station = c())
+#
+##Run line 92 is not including any other stations, run line 93 is includine more stations to selection
+WQ_stations_final <- WQ_Stations
+WQ_stations_final 
+WQ_data_t %>% subset(MonitoringLocationIdentifier %in% To_include$StationID) %>% mutate(Buffer = "Extra") %>% left_join(To_include)
