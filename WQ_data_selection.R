5d7c91f608337aeb8d8662c99b25fcfd242977de
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-01-23 15:03:57 -0500

    NN updates
    Code added to select WQ stations based on N closest stations. Output includes station assignment and distance from location.
    Word doc updated for option.

--- WQ_data_selection.R
+++ WQ_data_selection.R
@@ -11,23 +11,23 @@ if (!require("pacman")) {install.packages("pacman")}
 pacman::p_unlock()
 pacman::p_load(plyr, tidyverse, readxl, writexl, #Df manipulation, basic summary
                ggmap, tibble, zoo, measurements,
-               sf, raster, spData, 
+               sf, raster, spData, nngeo,
                tmap, tmaptools, htmltools, htmlwidgets,
                install = TRUE) 
 #
 #
 #
 ###Setup
-Estuary_code <- c("TB") #Two letter estuary code
-Data_source <- c("Portal") #"Portal", "WA" , or "FIM"
+Estuary_code <- c("SL") #Two letter estuary code
+Data_source <- c("Portal") #"Portal" or "WA". Code not currently updated for FIM"
 #
 #Years of possible data:
-Start_year <- c("2015")
-End_year <- c("2023")
+Start_year <- c("2000")
+End_year <- c("2022")
 #
 ##Years of desired data:
 Begin_data <- c("2015")
-End_data <- c("2023")
+End_data <- c("2022")
 #
 #
 ####Load Files#####
@@ -37,7 +37,8 @@ WQ_data <- as.data.frame(read_excel(paste0("Data/Raw_cleaned/", Estuary_code, "_
 #
 #
 #Fixed station locations
-Station_locations <- as.data.frame(read_excel("Data/Reference_data/Stations_area_selections.xlsx", na = c("NA", " ", "", "Z")))
+Station_locations <- as.data.frame(read_excel("Data/Reference_data/Stations_area_selections.xlsx", na = c("NA", " ", "", "Z"))) %>% 
+  subset(grepl(Estuary_code, Site))
 #
 ##Estuary area  
 Estuary_area <- st_read(paste0("KML/", Estuary_code, ".kml"))
@@ -61,7 +62,7 @@ Stations_selected <- Station_locations %>% subset(Station < 6)
 #
 #
 #
-####Station selection by distance - run only if selecting stations based on proximity to established locations ("Station_locations")####
+####Station selection - specified buffer distance - run only if selecting stations based on proximity to established locations ("Station_locations")####
 #
 F_Distance <- c(1500) #distance in meters
 S_Distance <- c(3000) #secondary distance for additional search area of potential stations 
@@ -142,7 +143,128 @@ write_xlsx(WQ_stations_final_df, paste0("Data/Raw_cleaned/", Estuary_code, "_", 
 #           
 #
 #
-####Output all stations####
+####Station selection - closest N stations####
+#
+##Selection continues from data narrowed by desired date range. If spatial selection is not needed, skip section.
+##Desired number of WQ stations per location
+Stations_N <- 3
+maxDist_m <- Inf #Maximum distance (in  meters) allowed in selection. If no max, set as Inf
+#
+Stations_t <- st_as_sf(Stations_selected, coords = c(3,4), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
+#All WQ data possible:
+WQ_data_t <- st_as_sf(WQ_selected, coords = c(9,8), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
+#
+if(Data_source == "Portal"){
+  #List of possible stations and their coordinates:
+  WQ_locations_t <- st_as_sf(WQ_selected %>% dplyr::select(MonitoringLocationIdentifier, LatitudeMeasure, LongitudeMeasure) %>% distinct(),
+                             coords = c(3,2), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
+  #
+  #Empty data frame to fill with selected data
+  WQ_closest_selected <- data.frame()
+  #
+  for(i in 1:nrow(Stations_t)){
+    #Determine closest N stations for each station
+    temp_station <- Stations_t$geometry[i]
+    Selected_WQstation_info <- st_nn(temp_station, WQ_locations_t, k = Stations_N, maxdist = maxDist_m, returnDist = TRUE)
+    #Create data frame of information of selected stations
+    Selected_WQstations <- data.frame(MonitoringLocationIdentifier = WQ_locations_t$MonitoringLocationIdentifier[Selected_WQstation_info$nn[[1]]], #Station IDs of closest stations
+                                      Distance = Selected_WQstation_info$dist[[1]],  #Distances from specified point
+                                      LocationID = (Stations_selected %>% unite(LocationID, Site, Station, sep = "_", remove = TRUE))[i,1]) #Location ID related to
+    #Limit WQ data to selected stations and add information to relate each station to the specified location
+    Selected_data <- left_join(WQ_data_t %>% filter(MonitoringLocationIdentifier %in% WQ_locations_t$MonitoringLocationIdentifier[Selected_WQstation_info$nn[[1]]]), Selected_WQstations)
+    #Combine filtered data to final output
+    WQ_closest_selected <<- rbind(WQ_closest_selected, Selected_data)
+    #Map of all possible stations and stations selected for each location
+    map <- tm_shape(Estuary_area) + tm_polygons() + #Estuary area
+      tm_shape(FL_outline) + tm_borders() + #Outline of shoreline
+      tm_shape(WQ_locations_t) + tm_dots(size = 2) + #Stations relation to estuary area
+      tm_shape(WQ_closest_selected) + #Stations selected per location
+        tm_dots(size = 1.25, col = "LocationID", legend.show = FALSE) + tm_facets(by = "LocationID", free.coords = FALSE)
+    print(map)
+  }
+} else if (Data_source == "WA"){
+    #Determine closest N stations for each station
+    temp_station <- Stations_t$geometry[i]
+    Selected_WQstation_info <- st_nn(temp_station, WQ_locations_t, k = Stations_N, maxdist = maxDist_m, returnDist = TRUE)
+    #Create data frame of information of selected stations
+    Selected_WQstations <- data.frame(StationID = WQ_locations_t$StationID[Selected_WQstation_info$nn[[1]]], #Station IDs of closest stations
+                                      Distance = Selected_WQstation_info$dist[[1]],  #Distances from specified point
+                                      LocationID = (Stations_selected %>% unite(LocationID, Site, Station, sep = "_", remove = TRUE))[i,1]) #Location ID related to
+    #Limit WQ data to selected stations and add information to relate each station to the specified location
+    Selected_data <- left_join(WQ_data_t %>% filter(StationID %in% WQ_locations_t$StationID[Selected_WQstation_info$nn[[1]]]), Selected_WQstations)
+    #Combine filtered data to final output
+    WQ_closest_selected <<- rbind(WQ_closest_selected, Selected_data)
+    #Map of all possible stations and stations selected for each location
+    map <- tm_shape(Estuary_area) + tm_polygons() + #Estuary area
+      tm_shape(FL_outline) + tm_borders() + #Outline of shoreline
+      tm_shape(WQ_locations_t) + tm_dots(size = 2) + #Stations relation to estuary area
+      tm_shape(WQ_closest_selected) + #Stations selected per location
+      tm_dots(size = 1.25, col = "LocationID", legend.show = FALSE) + tm_facets(by = "LocationID", free.coords = FALSE)
+    print(map)
+} else {
+  print(paste0("Code not yet written for data source ", Data_source))
+}
+rm(temp_station, Selected_WQstation_info, Selected_WQstations, Selected_data)
+#
+#
+#
+#Widget output to identify stations.
+if(Data_source == "Portal"){
+  (map <- tmap_leaflet(tm_shape(Estuary_area) + tm_polygons(col = "lightblue")+ #Estuary area
+                         tm_shape(FL_outline) + tm_borders()+ #Outline of shoreline
+                         tm_shape(WQ_locations_t) + tm_dots(size = 0.5, legend.show = FALSE, popup.vars = c("StationID" = "MonitoringLocationIdentifier"))+ #Possible stations
+                         tm_shape(WQ_closest_selected %>% dplyr::select(MonitoringLocationIdentifier, LocationID, Distance) %>% unique()) + 
+                          tm_dots(col = "red", size = 0.75, legend.show = TRUE, popup.vars = c("StationID" = "MonitoringLocationIdentifier", "LocationID" = "LocationID", "Distance" = "Distance"))+
+                         tm_layout(main.title = paste(Estuary_code, Data_source, "Closest", Stations_N,  "WQ Stations", Begin_data, "-", End_data, sep = " ")))) #Selected stations and buffer area
+} else if(Data_source == "WA"){
+  (map <- tmap_leaflet(tm_shape(Estuary_area) + tm_polygons(col = "lightblue")+ #Estuary area
+                         tm_shape(FL_outline) + tm_borders()+ #Outline of shoreline
+                         tm_shape(WQ_locations_t) + tm_dots(size = 0.5, legend.show = FALSE, popup.vars = c("StationID" = "StationID"))+ #Possible stations
+                         tm_shape(WQ_closest_selected %>% dplyr::select(StationID, LocationID, Distance) %>% unique()) + 
+                          tm_dots(col = "red", size = 0.75, legend.show = TRUE, popup.vars = c("StationID" = "StationID", "LocationID" = "LocationID", "Distance" = "Distance"))+
+                         tm_layout(main.title = paste(Estuary_code, Data_source, "Closest", Stations_N, "WQ Stations", Begin_data, "-", End_data, sep = " ")))) #Selected stations and buffer area
+} else {
+  print(paste0("Code not yet written for data source ", Data_source))
+}
+#
+#saveWidget(map, paste0("Maps/Station_selection/", Estuary_code, "_", Data_source,"_closest_stations_", Begin_data, "_", End_data, "_widget.html"))
+#
+# List of any stations to include or exclude from selection: need station ID and station of reference (both within "")- keep matched on one line/column
+To_include <- data.frame(StationID = c("21FLCOSP_WQX-32-03", "21FLHILL_WQX-28", "21FLHILL_WQX-25"),
+                         LocationID = c("LXN-1", "LXN-1", "LXN-1"))
+To_exclude <- data.frame(StationID = c("21FLTPA_WQX-G5SW0146", "21FLCOSP_WQX-45-03", "21FLCOSP_WQX-CENTRAL CANAL"),
+                         LocationID = c("LXN-4", "LXN-4", "LXN-4"))
+#
+##Run first line if not including or excluding any stations, run second line to include more stations to selection
+WQ_stations_final <- WQ_closest_selected
+if(Data_source == "Portal"){
+  WQ_stations_final <- rbind(WQ_closest_selected, 
+                             #Stations to include
+                             WQ_data_t %>% 
+                               subset(MonitoringLocationIdentifier %in% To_include$StationID) %>% mutate(Distance = "NA") %>% 
+                               left_join(To_include, by = c("MonitoringLocationIdentifier" = "StationID")))  %>%
+    #Stations to exclude
+    subset(!MonitoringLocationIdentifier %in% To_exclude$StationID)
+} else {
+  print(paste0("Code not yet written for data source ", Data_source))
+}
+#
+##Get coordinates into columns
+WQ_stations_final_df <- WQ_stations_final %>% st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>%
+  mutate(Longitude = st_coordinates(.)[,1],
+         Latitude = st_coordinates(.)[,2]) %>% dplyr::select(-geometry)
+#
+##Code (3-4 letters preferred) to identify project selected data is for:
+Project_code <- c("Cage")
+#
+##Export cleaned final data
+write_xlsx(WQ_stations_final_df, paste0("Data/Compiled_data/", Estuary_code, "_", Data_source, "_selected_", Project_code, "_", Begin_data, "_", End_data,".xlsx"), format_headers = TRUE)
+#
+#
+#
+#
+#
+####Output all stations possible####
 #
 WQ_data_t <- st_as_sf(WQ_selected, coords = c(9,8), crs = "+proj=longlat +datum=WGS84 +no_defs +type=crs") %>% st_transform(crs = st_crs(3086))
 #
