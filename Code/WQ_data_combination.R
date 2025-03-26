0b22492e15e5cf874ce94352c16d75fe94e48952
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-03-26 12:21:42 -0400

    Update WQ_data_combination.R
    Code to load and compile WQ data from Portal and FIM data selections.

--- Code/WQ_data_combination.R
+++ Code/WQ_data_combination.R
@@ -18,15 +18,32 @@ pacman::p_load(plyr, tidyverse, readxl, writexl, #Df manipulation, basic summary
 source("Code/WQ_functions.R")
 #
 #
-###Setup - specs####
+####Setup - specs####
 Estuary_code <- c("SL") #Two letter estuary code
 Data_sources <- c("Portal", "FIM") #List of all data sources to combine: "Portal", "FIM"
-Project_codes <- c("SLAllWQ") #List of any project code IDs used (portion before years)
-Data_selection_method <- c("NA") #List of data selection methods used: "NA", "selected_buffer", "closest_selected", "bounding_box", "name_selected"
+Project_codes <- c("SLAllWQ", "TEST") #List of any project code IDs used (file name portion before years)
+Data_selection_method <- c("bounding_box", NA) #List of data selection methods used: NA, "selected_buffer", "closest_selected", "bounding_box", "name_selected"
+Final_code <- c("SLHSI") #Short code to distinguish what data is compiled to be used for 
 #Years of data selected (same in all file names - request update if different years needed)
-Start_year <- c("1997")
-End_year <- c("2022")
+Start_year <- c("2000")
+End_year <- c("2023")
+#
+##Any notes to be add to metadata of output file. All should be entered as string of text
+Data_note <- c("Data collected for testing HSM modeling.")
 #
 #
 #END OF SET UP
+#
+####Load files####
+#
+Data_gather <- load_all_files()
+Data_gather$summary
+Data_gather$filenames
+#
+Final_data <- combine_data_sources()
+Final_data
+#
+#
+#
+#END OF SECTION
 #
