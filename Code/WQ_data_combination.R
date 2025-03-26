574bb2b2a7fb7076475c5f2df190dea549a24250
ELevine <149699489+E-Levine@users.noreply.github.com>
2025-03-26 09:03:32 -0400

    Update WQ_data_combination.R
    File setup code.

--- Code/WQ_data_combination.R
+++ Code/WQ_data_combination.R
@@ -20,20 +20,13 @@ source("Code/WQ_functions.R")
 #
 ###Setup - specs####
 Estuary_code <- c("SL") #Two letter estuary code
-Data_source <- c("FIM") #"Portal" or "WA". Code not currently updated for "FIM"
-#
-#Number of files to combine (Enter 1 if only one file. Current max is 3 files):
-Filtered_files <- c(1)
-#Years of possible data (from file names). Start and end years required for each file. Use 'NA" for any unused files:
+Data_sources <- c("Portal", "FIM") #List of all data sources to combine: "Portal", "FIM"
+Project_codes <- c("SLAllWQ") #List of any project code IDs used (portion before years)
+Data_selection_method <- c("NA") #List of data selection methods used: "NA", "selected_buffer", "closest_selected", "bounding_box", "name_selected"
+#Years of data selected (same in all file names - request update if different years needed)
 Start_year <- c("1997")
 End_year <- c("2022")
-Start_year_2 <- c(NA)
-End_year_2 <- c(NA)
-Start_year_3 <- c(NA)
-End_year_3 <- c(NA)
 #
-##Years of desired data:
-Begin_data <- c("2000")
-End_data <- c("2023")
 #
+#END OF SET UP
 #
